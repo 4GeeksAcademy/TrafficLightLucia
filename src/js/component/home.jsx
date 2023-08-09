e07c4e9bba7bf056cc63de56aa3fde284058fed5
Luica <138072443+lumartins311@users.noreply.github.com>
2023-08-09 23:43:03 +0000

    semafforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,36 +12,40 @@ const Home = () => {
 	console.log(luzRoja);
 	const [luzAmarilla,setluzamarilla]=useState("btn-warning")
 	const [luzVerde,setluzverde]=useState("btn-success")
+	
 
 	function cambiarRojo() {
 		if (luzRoja === "btn-danger") {
-			setluzroja("btn-danger border-5 border-light p-2 mb-2 border-opacity-50")
-			setluzamarilla("btn-warning")
-			setluzverde("btn-success")
+			setluzroja("btn-danger border-light border-5")
+			setluzamarilla("btn-secondary")
+			setluzverde("btn-secondary")
 		} else {
-			setluzroja("btn-danger")
+			setluzroja("btn-danger border-light border-5")
 			
 		}
 		
 	}
 	function cambiarAmarillo() {
 		if (luzAmarilla === "btn-warning") {
-			setluzamarilla("btn-warning border-5 border-light p-2 mb-2 border-opacity-50")
-			setluzverde("btn-success")
-			setluzroja("btn-danger")
+			setluzamarilla("btn-warning border-light border-5")
 		} else {
-			setluzamarilla("btn-warning")
+			setluzamarilla("btn-warning border-light border-5")
+			setluzverde("btn-secondary")
+			setluzroja("btn-secondary")
+			
+			
 		}
 		
 	}
 
+
 	function cambiarVerde() {
 		if (luzVerde === "btn-success") {
-			setluzverde("btn-success border-5 un border-light p-2 mb-2 border-opacity-50")
-			setluzamarilla("btn-warning")
-			setluzroja("btn-danger")
+			setluzverde("btn-success border-light border-5")
 		} else {
-			setluzverde("btn-success")
+			setluzverde("btn-success border-light border-5")
+			setluzamarilla("btn-secondary")
+			setluzroja("btn-secondary")
 		}
 		
 	}
@@ -49,9 +53,9 @@ const Home = () => {
 	
 	return (
 		<div className="text-center btn-group-vertical position-absolute top-50 start-50 translate-middle p-5 bg-dark">
-			<button href="#" className={"btn "+ luzRoja} onClick={cambiarRojo} style={{height:"100px", width:"100px", borderRadius:"75px", marginBottom:"10px"}}>
+			<button href="#" className={"btn "+ luzRoja} onClick={cambiarRojo} style={{ height:"100px", width:"100px", borderRadius:"75px", marginBottom:"10px"}}>
 			</button>
-			<button href="#" className={"btn "+ luzAmarilla } onClick={cambiarAmarillo} style={{height:"100px", width:"100px", borderRadius:"75px", marginBottom:"10px"}}>
+			<button href="#" className={"btn "+ luzAmarilla} onClick={cambiarAmarillo} style={{height:"100px", width:"100px", borderRadius:"75px", marginBottom:"10px"}}>
 			</button>
 			<button href="#" className={"btn "+ luzVerde } onClick={cambiarVerde} style={{height:"100px", width:"100px", borderRadius:"75px"}}>
 			</button>
